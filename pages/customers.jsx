6dd5c0e83145b0d92f21bb8d4bce10014991409a
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-01-23 21:03:13 -0800

    init commit

---
+++ pages/customers.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BsPersonFill, BsThreeDeotsVertical } from 'react-icons';
+import {data } from '../data/data.js'
+
+const customers = () => {
+    return (
+        <div>
+            <div>
+                <h2>Customers</h2>
+                <h2>Welcome Back, Clint</h2>
+            </div>
+        </div>
+    )
+}
+
+export default customers
